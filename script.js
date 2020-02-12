2e263fdecf9ae47aaa14488e852ad70c01f2fefe
sw1ckham <sphwick@gmail.com>
2020-02-12 12:37:49 +0000

    finished challenges

---
+++ script.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {
+ 
+     // .mouseenter
+     // .mouseleave
+     // .click 
+
+ 	$("#stream1_btn").on("click", function() {
+ 		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+	  	$(".stream1").addClass('highlight_stream');
+	});
+	$("#stream2_btn").on("click", function() {
+		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+	  	$(".stream2").addClass('highlight_stream');
+	});
+	$("#stream3_btn").on("click", function() {
+		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+	  	$(".stream3").addClass('highlight_stream');
+	});
+    $("h2").mouseenter(function (){
+        $("#hr_html").addClass("lightBlueBackground");
+        $("#hr_mysql").addClass("lightBlueBackground");
+        $("#hr_python").addClass("lightBlueBackground");
+        $("#hr_jquery").addClass("lightBlueBackground");
+        $("#hr_django").addClass("lightBlueBackground");
+        $("#hr_css").addClass("lightBlueBackground");
+    });  // Works on console but makes ALL of them light blue in the background.
+
+    $("#hr_html").mouseenter(function(){
+        $("#hr_html").css("font-size", "20px");
+    }); // WORKS ON CONSOLE 
+
+    $("p").click(function(){
+        $(".card_para").css("color", "red");
+    });  // Works on console!
+
+    $(".bottom_button").mouseenter(function(){
+        $("#container").css("background-color", "#000");
+    });
+    $(".bottom_button").mouseleave(function(){
+        $("#container").css("background-color", "#A9A9A9");
+    });  // Both of these work on the console 
+
+    $("#firstButton").click(function(){
+        $("#firstButton").hide(2000);
+    }); // Works on console
+
+    $("#secondButton").click(function(){
+        $("#secondPara").slideToggle();
+        $("#secondPara").slideToggle();
+    }); // Works on console
+
+    $("#thirdButton").mouseenter(function(){
+        $("#thirdButton").fadeTo(1000, 0.5);
+    });
+    $("#thirdButton").mouseleave(function(){
+        $("#thirdButton").fadeTo(1000, 1);
+    }); //Works on console
+
+
+
+
+}); 
