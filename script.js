47a0417d3d4492678fe84c964fe03eaf1576fbda
sw1ckham <sphwick@gmail.com>
2020-02-12 20:52:23 +0000

    Add traversing the Dom

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
  
-     // .mouseenter
-     // .mouseleave
-     // .click 
+//      // .mouseenter
+//      // .mouseleave
+//      // .click 
 
  	$("#stream1_btn").on("click", function() {
  		$(".stream1").removeClass('highlight_stream');
@@ -22,45 +22,72 @@ $(document).ready(function() {
 		$(".stream3").removeClass('highlight_stream');
 	  	$(".stream3").addClass('highlight_stream');
 	});
-    $("h2").mouseenter(function (){
-        $("#hr_html").addClass("lightBlueBackground");
-        $("#hr_mysql").addClass("lightBlueBackground");
-        $("#hr_python").addClass("lightBlueBackground");
-        $("#hr_jquery").addClass("lightBlueBackground");
-        $("#hr_django").addClass("lightBlueBackground");
-        $("#hr_css").addClass("lightBlueBackground");
-    });  // Works on console but makes ALL of them light blue in the background.
+//     $("h2").mouseenter(function (){
+//         $("#hr_html").addClass("lightBlueBackground");
+//         $("#hr_mysql").addClass("lightBlueBackground");
+//         $("#hr_python").addClass("lightBlueBackground");
+//         $("#hr_jquery").addClass("lightBlueBackground");
+//         $("#hr_django").addClass("lightBlueBackground");
+//         $("#hr_css").addClass("lightBlueBackground");
+//     });  // Works on console but makes ALL of them light blue in the background.
 
-    $("#hr_html").mouseenter(function(){
-        $("#hr_html").css("font-size", "20px");
-    }); // WORKS ON CONSOLE 
+//     $("#hr_html").mouseenter(function(){
+//         $("#hr_html").css("font-size", "20px");
+//     }); // WORKS ON CONSOLE 
 
-    $("p").click(function(){
-        $(".card_para").css("color", "red");
-    });  // Works on console!
+//     $("p").click(function(){
+//         $(".card_para").css("color", "red");
+//     });  // Works on console!
 
-    $(".bottom_button").mouseenter(function(){
-        $("#container").css("background-color", "#000");
+//     $(".bottom_button").mouseenter(function(){
+//         $("#container").css("background-color", "#000");
+//     });
+//     $(".bottom_button").mouseleave(function(){
+//         $("#container").css("background-color", "#A9A9A9");
+//     });  // Both of these work on the console 
+
+//     $("#firstButton").click(function(){
+//         $("#firstButton").hide(2000);
+//     }); // Works on console
+
+//     $("#secondButton").click(function(){
+//         $("#secondPara").slideToggle();
+//         $("#secondPara").slideToggle();
+//     }); // Works on console
+
+//     $("#thirdButton").mouseenter(function(){
+//         $("#thirdButton").fadeTo(1000, 0.5);
+//     });
+//     $("#thirdButton").mouseleave(function(){
+//         $("#thirdButton").fadeTo(1000, 1);
+//     }); //Works on console
+
+//     $(".card_para").click(function(){
+//         $(".card_para").children("a").css("background-color", "yellow");
+//     });   // traversing through the DOM - actioning anchor tags without a class or ID 
+
+//     /*--- TRAVERSING THROUGH THE DOM SIDEWAYS --- */
+
+//     $(".card_image").click(function(){
+//         var someCards = $(this).next();
+//         someCards.next().slideDown();
+//     }); // working fine for 4 of the cards
+
+    /* TUTOR TO LOOK AT */
+
+    $(".card").click(function(){
+        $(this).toggleClass("pinkBackground");
+    });  // Works on console 
+
+    /* difficult challenge... */
+
+    $("#select").clicked(function(){
+        $(".card:not(.highlight_stream)").hide();
     });
-    $(".bottom_button").mouseleave(function(){
-        $("#container").css("background-color", "#A9A9A9");
-    });  // Both of these work on the console 
 
-    $("#firstButton").click(function(){
-        $("#firstButton").hide(2000);
-    }); // Works on console
-
-    $("#secondButton").click(function(){
-        $("#secondPara").slideToggle();
-        $("#secondPara").slideToggle();
-    }); // Works on console
-
-    $("#thirdButton").mouseenter(function(){
-        $("#thirdButton").fadeTo(1000, 0.5);
+    $("#all").clicked(function(){
+        $(".card").show();
     });
-    $("#thirdButton").mouseleave(function(){
-        $("#thirdButton").fadeTo(1000, 1);
-    }); //Works on console
 
 
 
